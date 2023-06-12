292de1f4ad5fc317908c448519480d3e50fb8fd3
coder-bendany <bendany1130@gmail.com>
2023-06-12 17:20:50 +0900

    test

---
+++ demo/src/main/resources/db/h2/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS TODOS (
+    id long NOT NULL AUTO_INCREMENT,
+    title varchar(100) NOT NULL,
+    todoOrder int NOT NULL,
+    completed boolean NOT NULL,
+    PRIMARY KEY (MESSAGE_ID)
+);
+
+
